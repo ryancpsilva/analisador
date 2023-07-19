3590320339cadf502c76d77f4d20a9d2e97eb69f
ryancpsilva <128498481+ryancpsilva@users.noreply.github.com>
2023-07-19 19:50:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let add = document.getElementById('txtadd')
 let list = document.getElementById('list')
+let res = document.getElementById('res')
 let arr = []
 
 function isnumero(n) {
@@ -24,28 +25,40 @@ function adicionar() {
         item.text = `O valor ${add.value} foi adicionado`
         list.appendChild(item)
         arr.push(Number(add.value))
+        res.innerHTML = ''
     } else {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
    }
+   add.value = ''
+   add.focus()
 }
 function finalizar() {
-    let res = document.getElementById('res')
-    res.innerHTML = ''
     if (arr.length == 0) {
         window.alert('[ERRO] Por favor, adicione algum número!')
         list.innerHTML = ''
+        res.innerHTML = ''
     } else {
-        
-        arr.sort()
-        res.innerHTML += `<p>Ao todo temos ${arr.length} números adicionados</p>`
-        res.innerHTML += `<p>O maior valor informado foi ${arr[arr.length-1]}</p>`
-        res.innerHTML += `<p>O menor valor informado foi ${arr[0]}</p>`
-        let c = 0
-        for (let i in arr) {
-            c += Number(arr[i])
+        let tot = arr.length
+        let maior = arr[0]
+        let menor = arr[0]
+        let soma = 0
+        let media = 0
+        for (let pos in arr) {
+            soma += arr[pos]
+            if (arr[pos] > maior) {
+                maior = arr[pos]
+            } 
+            if (arr[pos] < menor) {
+                menor = arr[pos]
+            }
         }
-        res.innerHTML += `<p> A soma de todos os valores é igual a ${c}</p>`
-        res.innerHTML += `<p> A média de todo os valores é igual a ${c/arr.length}`
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
         arr=[]
     }
 }
